ab21f17671397b686411def4d2cdbd1e75230afa
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-08-22 12:00:36 +0200

    fixed minor issues

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,16 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const Course = require('../models/course');
+const Review = require('../models/review');
 
 /* GET home page. */
 router.get('/api/users', function(req, res, next) {
     res.status(200);
-    User.findOne({}, function(err, users){
+    User.find({}, function(err, users){
         if(err) return next(err);
         res.json(users);
+        console.log(users)
     });
 
 });
@@ -16,6 +19,7 @@ router.get('/api/users', function(req, res, next) {
 router.post('/api/users', function(req, res, next) {
     // use schema's `create` method to insert document into Mongo
     User.create(req.body, function (err, user) {
+        console.log(req.body)
         if (err) {
             return next(err);
         }
